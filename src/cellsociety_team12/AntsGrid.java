9447ff58063efd1c22c86d7214946d055e07aa6f
Dennis Ling <dennis.ling@duke.edu>
2017-02-12 00:04:57 -0500

    update cell and grid for ANT sim

--- src/cellsociety_team12/AntsGrid.java
+++ src/cellsociety_team12/AntsGrid.java
@@ -1,17 +1,20 @@
 package cellsociety_team12;
+import java.util.ArrayList;
+
+import cells.AntsCell;
 import cells.Cell;
+import cells.WatorCell;
+import javafx.scene.shape.Rectangle;
 
 public class AntsGrid extends Grid{
 
 	public AntsGrid(int dimensions, Game game) {
 		super(dimensions, game);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected Cell cellType(int x, int y) {
-		// TODO Auto-generated method stub
-		return null;
+		return new AntsCell(x, y, "ground", new Rectangle(), new ArrayList<Boolean>(), new ArrayList<Boolean>(), new int[4]);
 	}
 	
 }
