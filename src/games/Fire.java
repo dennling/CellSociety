9855ccc858409625e3f41e3db616282f0ab91dcd
Dennis Ling <dennis.ling@duke.edu>
2017-02-16 02:51:54 -0500

    Add masterpiece: Fire and Game

--- src/games/Fire.java
+++ src/games/Fire.java
@@ -1,3 +1,14 @@
+//This entire file is part of my masterpiece.
+//Dennis Ling
+
+/* 
+ * This class is a subclass of Game (the other part of my masterpiece).
+ * This is well designed because it represents what I've learned about using super and subclasses to have extensible, flexible code.
+ * This code is one example of how to extend our program to allow for more simulations.
+ * The gameLogic method (along with all of the other methods) is also concise and understandable, with appropriate variable and method names.
+ * Both of my master piece codes also continue from what I've learned from the last project to not have public non-static variables, and to not have static methods.
+ */
+
 package games;
 
 import cells.Cell;
@@ -5,9 +16,8 @@ import cellsociety_team12.GameData;
 import grids.FireGrid;
 import grids.Grid;
 
-/**
- * Class for the Fire Game
- *
+/*
+ * Class for the Fire simulation, subclass of game
  */
 public class Fire extends Game{
 
@@ -18,21 +28,23 @@ public class Fire extends Game{
 		fireChance = data.getProb();
 	}
 
+	/*
+	 * Handles the logic of how cells are updated in the Fire simulation
+	 */
 	@Override
 	public void gameLogic(Cell currentCell) {
 		Cell[] neighbors = currentCell.getNeighbors();
-
-		if(currentCell.getType().equals("tree")){
+		String type = currentCell.getType();
+		if(type.equals("tree")){
 			for(int i=0; i<neighbors.length; i++){ 
 				if(neighbors[i].getType().equals("fire")){
-					double roll = Math.random(); 
-					if(roll<=fireChance){
+					if(Math.random()<=fireChance){
 						currentCell.setType("fire"); 
 					}
 				}
 			}
 		}
-		else if(currentCell.getType().equals("fire")){
+		else if(type.equals("fire")){
 			currentCell.setType("empty");
 		}
 	}
--- src/games/Game.java
+++ src/games/Game.java
@@ -1,3 +1,15 @@
+//This entire file is part of my masterpiece.
+//Dennis Ling
+
+/*
+ * This class is the superclass for all of our simulators (games). It primarily handles the game logic, but also specifies the initial configuration of cells.
+ * This code is well designed because it builds upon my learning of inheritance, especially as it pertains to superclasses and subclasses.
+ * The connections that this class has with the rest of the program also shows how well designed the flow of the program is.
+ * This code contains the necessary methods that the subclasses will either override or use exactly as specified.
+ * This method also does not use public variables, but instead contains public methods to access these variables.
+ * To fully appreciate the knowledge I've gained in doing this project, the other segment of my masterpiece (Fire) is an example of a well-coded subclass.
+ */
+
 package games;
 
 import java.util.Random;
@@ -7,12 +19,11 @@ import cellsociety_team12.GameData;
 import cellsociety_team12.XMLException;
 import grids.Grid;
 
-/**
+/*
  * Superclass for game. Primary role of game classes is to specify game logic and set initial 
  * configuration settings. 
  *
  */
-
 public abstract class Game {
 	
 	private Grid myGrid;
@@ -27,8 +38,8 @@ public abstract class Game {
 		myGrid.updateCellNeighbors();
 	}
 	
-	/**
-	 * Creates initial positions of significant cells based on XML input, if non exist, create default
+	/*
+	 * Creates initial positions of significant cells based on XML input, if none exist, create default
 	 * cells
 	 */
 	public void setInitialPositions() {	
@@ -44,26 +55,24 @@ public abstract class Game {
 				int y = Integer.parseInt(each[1]);
 				if (x < getGrid().getRow(0).length && x >= 0 && y < getGrid().getRow(0).length && y >= 0){ 
 					getGrid().getCell(x,y).setType(each[2]);
-				}
-				
+				}	
 			}
 		}
 	}
 	
-	
 	protected abstract Grid createGrid(int dimensions, String cellShape);
 	
-	/**
+	/*
 	 * Determines how the cells react to their neighbors according to the rules of the game
 	 */
 	public abstract void gameLogic(Cell currentCell);
 	
-	/**
-	 * if no initial parameters in XML, randomizes
+	/*
+	 * If no initial parameters in XML, randomizes
 	 */
 	protected abstract void setDefaultPositions(GameData data);
 	
-	/**
+	/*
 	 * Generates a random placement on the grid 
 	 */
 	protected void randomCellGenerator(String type, GameData data) {
